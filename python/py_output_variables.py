ca6dd75bc8e731df1478a6fdc93e2931ee41f8da
eric-spu <bscnrb591520@spu.ac.ke>
2023-11-12 20:42:41 +0300

    Py output varaibles

---
+++ python/py_output_variables.py
@@ -0,0 +1,39 @@
+print("Python - Output variables...")
+
+"""
+Output Variables
+The Python print() function is often used to output variables.
+In the print() function, you output multiple variables, separated by a comma:
+You can also use the + operator to output multiple variables:
+
+Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+For numbers, the + character works as a mathematical operator:
+
+The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types:
+
+"""
+x = "Python is awesome."
+print(x)
+
+print("\nprint, mutiple output ")
+k = "Python"
+i = "is"
+j = "awesome"
+print(k, i, j) # output multiple variables, separated by a comma
+
+print('\n')
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+print("\n")
+x = 5
+y = 10
+print(x + y) # the + character works as a mathematical operator:
+
+
+x = 5
+y = "John"
+print("\n", x, y)
+
